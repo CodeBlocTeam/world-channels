08d0c24b3a1a4e528bd9f54b5506ddaabd5dbe8e
taiite <hubert.hirtz@laposte.net>
2017-07-04 21:36:53 +0200

    Added doc comments

--- src/main/java/io/github/codeblocteam/WorldChannels.java
+++ src/main/java/io/github/codeblocteam/WorldChannels.java
@@ -23,6 +23,7 @@ TODO: Pouvoir grouper des mondes qui partagent le même channel (overworld, neth
 TODO: Ajouter une commande pour envoyer un message à tout le serveur
 TODO: Ajouter une commande pour envoyer un message à des mondes spécifiques
 TODO: Ajouter un callback au /reload
+TODO: Créer un service pour gérer les channels
  */
 
 @Plugin(
@@ -32,11 +33,17 @@ TODO: Ajouter un callback au /reload
 )
 public class WorldChannels {
 
+    /** Keeping track of all the channels created. */
     private Map<String, MessageChannel> channels;
 
+    /** A SFL4J logger. */
     @Inject
     private Logger logger;
 
+    /**
+     * Create a channel for the given world.
+     * @param world
+     */
     private void loadWorldChannel(World world) {
         String name = world.getName();
         MessageChannel channel = MessageChannel.world(world);
@@ -45,11 +52,22 @@ public class WorldChannels {
         channels.putIfAbsent(name, channel);
     }
 
+    /**
+     * Delete the channel associated with the given world.
+     * @param world
+     */
     private void unloadWorldChannel(World world) {
         logger.info("Unloading world channel of " + world.getName());
         channels.remove(world.getName());
     }
 
+    /**
+     * Change the channel the player is talking into.
+     *
+     * @param player The player whom channel will change.
+     * @param world The world associated with the channel the player
+     *              will change to.'
+     */
     private void updatePlayerChannel(Player player, World world) {
         MessageChannel channel = channels.get(world.getName());
 
